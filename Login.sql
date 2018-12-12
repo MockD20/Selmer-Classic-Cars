400e22ebcbaf2e580cc38c609558b535ebecf660
Damon Mock <43882909+MockD20@users.noreply.github.com>
2018-12-12 09:54:30 -0600

    Create Login.sql

---
+++ Login.sql
@@ -0,0 +1,8 @@
+Create Database Users;
+Create Table Login  (
+  Username varchar(20),
+  Password varchar(20)
+);
+Insert Into Login   (Username, Password)
+values ('SelmerCars18', 'classic9294')
+SELECT * FROM 'Login';
